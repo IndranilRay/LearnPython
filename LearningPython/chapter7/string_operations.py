afcee25b7e6d3a2f27202eedf88b52930eeae7e3
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-07-22 13:34:26 +0530

    string operations

---
+++ LearningPython/chapter7/string_operations.py
@@ -0,0 +1,15 @@
+# skipping items
+S = 'abcdefghijklmnop'
+print(S[1:10:2])
+
+# skipping without bounds
+print(S[::2])
+
+# reversing a string
+print(S[::-1])
+
+# with negative strides meaning of first two bounds are reversed
+print(S[5:2:-1])
+
+
+
