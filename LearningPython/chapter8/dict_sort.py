ea7831b228b013e8010afd7523411d2d87215b75
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-07-23 19:56:46 +0530

    Dictionary sorting

---
+++ LearningPython/chapter8/dict_sort.py
@@ -0,0 +1,28 @@
+# sorting a dictionary
+
+# Method 1
+D = {'a': 1, 'b': 2, 'c': 3}
+ks = D.keys()
+
+try:
+    ks.sort()
+except AttributeError:
+    ks = list(ks)
+    ks.sort()
+    for k in ks:
+        print(k, D[k])
+
+# Method 2
+D = {'b': 2, 'c': 3, 'a': 1}
+for k in sorted(D.keys()):
+    print(k,D[k])
+
+
+
+
+
+
+
+
+
+
