416e376398d4a075621270888d867faeb251a012
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-08-01 23:21:09 +0530

    Find all odd numbers without using 'Continue' statement

--- LearningPython/chapter13/continue.py
+++ LearningPython/chapter13/continue.py
@@ -4,9 +4,18 @@ Using continue statement in  a while loop
 x = 10
 
 # Find all even numbers between 1 to 10
-# version 1 using continue statements
+# version 1 : using continue statements
 
 while x:
     x -= 1
     if x % 2 != 0: continue
-    print("%d is odd" % x)
+    print("%d is even" % x)
+
+# Find all odd numbers between 1 to 10
+# version2 : using conditional statement
+
+y = 10
+while y:
+    y -= 1
+    if y % 2 != 0:
+        print(y, end=' ')
